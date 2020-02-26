21bf14435c82916e373b8357b7df4193c0a84f6f
shev4uk <shev4ukyuri@gmail.com>
2020-02-26 21:56:20 +0200

    leeson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'course';
+  title = 'Course';
+  counter = 1;
+  stateDropDown = false;
+  products = [
+    {
+      id: 1,
+      name: 'product 1',
+      price: 36
+    },
+    {
+      id: 2,
+      name: 'product 2',
+      price: 46
+    },
+    {
+      id: 3,
+      name: 'product 3',
+      price: 31
+    }
+  ]
+
+  changeTitle() {
+    this.title = 'Lesson';
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
+
+  toggleDropDown() {
+    this.stateDropDown = !this.stateDropDown;
+  }
 }

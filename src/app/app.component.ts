6c144366109f9bc327f5c8b74a8ed8f172131a32
shev4uk <shev4ukyuri@gmail.com>
2020-03-02 22:05:27 +0200

    typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+enum ResponseCode {
+    Ok = 200,
+    Redirect = 301,
+    NotFound = 404
+}
+
+type Admin = {
+  name: string,
+  password: string,
+  role: number,
+  age?: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'course';
+  private title = 'course';
+  number: number;
+  arr: number[] = [1, 5, 10];
+  user: {name: string, age: number} = {
+    name: 'Bob',
+    age: 23
+  }; 
+  admin: Admin = {
+    name: 'Admin',
+    password: '123',
+    role: 777
+  }
+  private readonly code: ResponseCode = ResponseCode;
+
+  ngOnInit() {
+    this.changeString();
+    this.number = 10;
+
+    console.log(this.code.Ok);
+    console.log(this.code[200]);
+  }
+
+  changeString(): string {
+    return 'string';
+  }
+
+  private privateMethod() {
+    this.number = 25;
+  }
 }
